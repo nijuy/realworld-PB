c5fcc03fbf6ba1d4172d667849f745d696ec3f11
nijuy <youjin6325@naver.com>
2023-05-21 23:08:09 +0900

    chore: eslint, prettier 설정

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,10 @@
 module.exports = {
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
+    'airbnb-base',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'prettier',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
@@ -11,4 +12,4 @@ module.exports = {
   rules: {
     'react-refresh/only-export-components': 'warn',
   },
-}
+};
