71a3bfdb7f8f30ea564717956f64f21c1f72bcc0
nijuy <youjin6325@naver.com>
2023-06-02 22:34:02 +0900

    feat(App): recoil 적용

--- src/App.tsx
+++ src/App.tsx
@@ -8,22 +8,25 @@ import Profile from './components/pages/Profile';
 import Settings from './components/pages/Settings';
 import Signup from './components/pages/Signup';
 import Signin from './components/pages/Signin';
+import { RecoilRoot } from 'recoil';
 
 function App() {
   return (
     <>
-      <HashRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/article:URLSlug" element={<Article />} />
-          <Route path="/editor" element={<CreateArticle />} />
-          <Route path="/editor:URLSlug" element={<EditArticle />} />
-          <Route path="/settings" element={<Settings />} />
-          <Route path="/profile/:userId" element={<Profile />} />
-          <Route path="/login" element={<Signin />} />
-          <Route path="/register" element={<Signup />} />
-        </Routes>
-      </HashRouter>
+      <RecoilRoot>
+        <HashRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/article:URLSlug" element={<Article />} />
+            <Route path="/editor" element={<CreateArticle />} />
+            <Route path="/editor:URLSlug" element={<EditArticle />} />
+            <Route path="/settings" element={<Settings />} />
+            <Route path="/profile/:userId" element={<Profile />} />
+            <Route path="/login" element={<Signin />} />
+            <Route path="/register" element={<Signup />} />
+          </Routes>
+        </HashRouter>
+      </RecoilRoot>
     </>
   );
 }
