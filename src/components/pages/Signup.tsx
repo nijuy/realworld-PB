09fa58e12af6a17f5d86f581a7e50dfab443730b
nijuy <youjin6325@naver.com>
2023-06-16 21:11:30 +0900

    feat: a 태그 href 걸기 (login 페이지로)

--- src/components/pages/Signup.tsx
+++ src/components/pages/Signup.tsx
@@ -72,7 +72,7 @@ const Signup = () => {
               <div className="col-md-6 offset-md-3 col-xs-12">
                 <h1 className="text-xs-center">Sign Up</h1>
                 <p className="text-xs-center">
-                  <a href="">Have an account?</a>
+                  <a href="#/login">Have an account?</a>
                 </p>
 
                 {signupStatusData && !signupStatusData.signupStatus && (
