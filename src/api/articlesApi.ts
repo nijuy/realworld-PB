cebce0ce660077ea6b4adef68674c43a1a093672
nijuy <youjin6325@naver.com>
2023-06-20 19:07:18 +0900

    fix: getGlobalFeed url 수정 부분 고치기

--- src/api/articlesApi.ts
+++ src/api/articlesApi.ts
@@ -6,8 +6,9 @@ export const articleApi = {};
 export const feedApi = {
   getGlobalFeed: (offset?: number, tag?: string) => {
     let url = '/articles?limit=10';
-    if (offset) url + `&offset=${offset}`;
-    if (tag) url + `&tag=${tag}`;
+    if (offset) url += `&offset=${offset}`;
+    if (tag) url += `&tag=${tag}`;
+
     const response = Axios.get<IFeedData>(url);
     return response;
   },
