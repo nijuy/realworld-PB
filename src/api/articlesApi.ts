536c7c3a5485aab3360e5f68a16f6ef075ac4c97
nijuy <youjin6325@naver.com>
2023-06-20 17:34:13 +0900

    refactor: getGlobalFeed 인자 추가, url 수정

--- src/api/articlesApi.ts
+++ src/api/articlesApi.ts
@@ -4,8 +4,11 @@ import { Axios } from './api';
 export const articleApi = {};
 
 export const feedApi = {
-  getGlobalFeed: () => {
-    const response = Axios.get<IFeedData>('/articles');
+  getGlobalFeed: (offset?: number, tag?: string) => {
+    let url = '/articles?limit=10';
+    if (offset) url + `&offset=${offset}`;
+    if (tag) url + `&tag=${tag}`;
+    const response = Axios.get<IFeedData>(url);
     return response;
   },
 };
