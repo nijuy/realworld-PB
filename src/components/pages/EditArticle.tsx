060d90c3a6f490a88c53075a3e43cdaac6c62e28
nijuy <youjin6325@naver.com>
2023-06-28 15:42:52 +0900

    feat: 글 내용 가져오기, 템플릿 수정하기

--- src/components/pages/EditArticle.tsx
+++ src/components/pages/EditArticle.tsx
@@ -1,6 +1,29 @@
+import { useParams } from 'react-router-dom';
 import Layout from '../layout/Layout';
+import { useEffect, useState } from 'react';
+import { articleApi } from '../../api/articlesApi';
+import { ISingleArticleResponse } from '../../types/articleApi.type';
 
 const EditArticle = () => {
+  const slug = useParams().URLSlug;
+
+  const [article, setArticle] = useState<ISingleArticleResponse>();
+
+  const getArticle = async () => {
+    try {
+      if (slug !== undefined) {
+        const response = await articleApi.read(slug);
+        setArticle(response.data);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getArticle();
+  }, [slug]);
+
   return (
     <Layout>
       <div className="editor-page">
@@ -14,6 +37,7 @@ const EditArticle = () => {
                       type="text"
                       className="form-control form-control-lg"
                       placeholder="Article Title"
+                      defaultValue={article?.article.title}
                     />
                   </fieldset>
                   <fieldset className="form-group">
@@ -21,6 +45,7 @@ const EditArticle = () => {
                       type="text"
                       className="form-control"
                       placeholder="What's this article about?"
+                      defaultValue={article?.article.description}
                     />
                   </fieldset>
                   <fieldset className="form-group">
@@ -28,11 +53,24 @@ const EditArticle = () => {
                       className="form-control"
                       rows={8}
                       placeholder="Write your article (in markdown)"
+                      defaultValue={article?.article.body}
                     ></textarea>
                   </fieldset>
                   <fieldset className="form-group">
                     <input type="text" className="form-control" placeholder="Enter tags" />
-                    <div className="tag-list"></div>
+                    <div className="tag-list">
+                      {article?.article.tagList &&
+                        article?.article.tagList.map((tagData, index) => (
+                          <span
+                            key={index}
+                            ng-repeat="tag in $ctrl.article.tagList"
+                            className="tag-default tag-pill ng-binding ng-scope"
+                          >
+                            <i className="ion-close-round" ng-click="$ctrl.removeTag(tag)"></i>
+                            {tagData}
+                          </span>
+                        ))}
+                    </div>
                   </fieldset>
                   <button className="btn btn-lg pull-xs-right btn-primary" type="button">
                     Publish Article
