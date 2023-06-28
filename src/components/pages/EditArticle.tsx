1c9f0d0baf84451d5c3f8df501e269b38bc79948
nijuy <youjin6325@naver.com>
2023-06-28 15:43:23 +0900

    feat: 글 수정 기능

--- src/components/pages/EditArticle.tsx
+++ src/components/pages/EditArticle.tsx
@@ -1,6 +1,6 @@
 import { useParams } from 'react-router-dom';
 import Layout from '../layout/Layout';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { articleApi } from '../../api/articlesApi';
 import { ISingleArticleResponse } from '../../types/articleApi.type';
 
@@ -8,16 +8,61 @@ const EditArticle = () => {
   const slug = useParams().URLSlug;
 
   const [article, setArticle] = useState<ISingleArticleResponse>();
+  const [tagList, setTagList] = useState<string[]>();
+
+  const titleRef = useRef<HTMLInputElement>(null);
+  const descriptionRef = useRef<HTMLInputElement>(null);
+  const bodyRef = useRef<HTMLTextAreaElement>(null);
+  const tagRef = useRef<HTMLInputElement>(null);
 
   const getArticle = async () => {
     try {
       if (slug !== undefined) {
         const response = await articleApi.read(slug);
         setArticle(response.data);
+        setTagList(response.data.article.tagList);
       }
     } catch (error) {
       console.log(error);
     }
+  };
+
+  const updateArticle = async (formEvent: React.FormEvent<HTMLFormElement>) => {
+    formEvent.preventDefault();
+    try {
+      if (slug !== undefined) {
+        const articleData = {
+          title: titleRef.current!.value,
+          description: descriptionRef.current!.value,
+          body: bodyRef.current!.value,
+          tagList: tagList || [],
+        };
+        await articleApi.update(slug, { article: articleData });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const addTag = () => {
+    const newTag = tagRef.current?.value || '';
+
+    if (newTag === '') {
+      alert('태그를 입력해주세요');
+      return;
+    }
+
+    if (tagList!.includes(newTag)) {
+      alert('이미 추가된 태그입니다');
+      return;
+    }
+
+    setTagList((prevState) => [...prevState!, newTag]);
+    tagRef.current!.value = '';
+  };
+
+  const removeTag = (removedTag: string) => {
+    setTagList((prevState) => prevState!.filter((tag) => tag !== removedTag));
   };
 
   useEffect(() => {
@@ -30,13 +75,14 @@ const EditArticle = () => {
         <div className="container page">
           <div className="row">
             <div className="col-md-10 offset-md-1 col-xs-12">
-              <form>
+              <form onSubmit={updateArticle}>
                 <fieldset>
                   <fieldset className="form-group">
                     <input
                       type="text"
                       className="form-control form-control-lg"
                       placeholder="Article Title"
+                      ref={titleRef}
                       defaultValue={article?.article.title}
                     />
                   </fieldset>
@@ -45,6 +91,7 @@ const EditArticle = () => {
                       type="text"
                       className="form-control"
                       placeholder="What's this article about?"
+                      ref={descriptionRef}
                       defaultValue={article?.article.description}
                     />
                   </fieldset>
@@ -53,26 +100,50 @@ const EditArticle = () => {
                       className="form-control"
                       rows={8}
                       placeholder="Write your article (in markdown)"
+                      ref={bodyRef}
                       defaultValue={article?.article.body}
                     ></textarea>
                   </fieldset>
                   <fieldset className="form-group">
-                    <input type="text" className="form-control" placeholder="Enter tags" />
+                    <div style={{ display: 'flex' }}>
+                      <input
+                        type="text"
+                        className="form-control"
+                        placeholder="Enter tags"
+                        ref={tagRef}
+                      />
+                      <button
+                        style={{
+                          marginLeft: '20px',
+                        }}
+                        className="btn btn-md pull-xs-right btn-primary"
+                        type="button"
+                        onClick={addTag}
+                      >
+                        Add Tag
+                      </button>
+                    </div>
                     <div className="tag-list">
-                      {article?.article.tagList &&
-                        article?.article.tagList.map((tagData, index) => (
+                      {tagList &&
+                        tagList.map((tagData, index) => (
                           <span
                             key={index}
                             ng-repeat="tag in $ctrl.article.tagList"
                             className="tag-default tag-pill ng-binding ng-scope"
                           >
-                            <i className="ion-close-round" ng-click="$ctrl.removeTag(tag)"></i>
+                            <i
+                              className="ion-close-round"
+                              ng-click="$ctrl.removeTag(tag)"
+                              onClick={() => {
+                                removeTag(tagData);
+                              }}
+                            ></i>
                             {tagData}
                           </span>
                         ))}
                     </div>
                   </fieldset>
-                  <button className="btn btn-lg pull-xs-right btn-primary" type="button">
+                  <button className="btn btn-lg pull-xs-right btn-primary" type="submit">
                     Publish Article
                   </button>
                 </fieldset>
