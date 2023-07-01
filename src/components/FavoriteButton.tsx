1b69444f34eb2e1645cf9f10b3c7df99ec00a2cb
nijuy <youjin6325@naver.com>
2023-07-01 20:45:07 +0900

    feat: 비로그인 사용자가 클릭 시 signup 으로 이동

--- src/components/FavoriteButton.tsx
+++ src/components/FavoriteButton.tsx
@@ -1,12 +1,21 @@
 import { useState } from 'react';
 import { IArticle } from '../types/articleApi.type';
 import { articleApi } from '../api/articlesApi';
+import { getToken } from '../services/TokenService';
+import { useNavigate } from 'react-router-dom';
 
 const FavoriteButton = ({ article }: { article: IArticle }) => {
   const [favoriteCount, setFavoriteCount] = useState(article.favoritesCount);
   const [favorited, setFavorited] = useState(article.favorited);
 
+  const navigate = useNavigate();
+
   const onClickFavoriteButton = async () => {
+    if (!getToken()) {
+      navigate('/register');
+      return;
+    }
+
     if (favorited) {
       setFavoriteCount(favoriteCount - 1);
       setFavorited(false);
