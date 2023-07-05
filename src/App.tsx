248fde594b7d5fbf8e5f144b7a55e304176cbfc0
nijuy <youjin6325@naver.com>
2023-07-05 20:59:35 +0900

    chore: RecoilContainer 수정

--- src/App.tsx
+++ src/App.tsx
@@ -8,25 +8,22 @@ import Profile from './components/pages/Profile';
 import Settings from './components/pages/Settings';
 import Signup from './components/pages/Signup';
 import Signin from './components/pages/Signin';
-import { RecoilRoot } from 'recoil';
 
 function App() {
   return (
     <>
-      <RecoilRoot>
-        <HashRouter>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/article/:URLSlug" element={<Article />} />
-            <Route path="/editor" element={<CreateArticle />} />
-            <Route path="/editor/:URLSlug" element={<EditArticle />} />
-            <Route path="/settings" element={<Settings />} />
-            <Route path="/profile/:username" element={<Profile />} />
-            <Route path="/login" element={<Signin />} />
-            <Route path="/register" element={<Signup />} />
-          </Routes>
-        </HashRouter>
-      </RecoilRoot>
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/article/:URLSlug" element={<Article />} />
+          <Route path="/editor" element={<CreateArticle />} />
+          <Route path="/editor/:URLSlug" element={<EditArticle />} />
+          <Route path="/settings" element={<Settings />} />
+          <Route path="/profile/:username" element={<Profile />} />
+          <Route path="/login" element={<Signin />} />
+          <Route path="/register" element={<Signup />} />
+        </Routes>
+      </HashRouter>
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import { RecoilRoot } from 'recoil';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const queryClient = new QueryClient();
@@ -9,7 +10,9 @@ const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <RecoilRoot>
+        <App />
+      </RecoilRoot>
     </QueryClientProvider>
   </React.StrictMode>,
 );
