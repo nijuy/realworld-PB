15012fd0c9e7f8757478a7574f4945d26b79e914
nijuy <youjin6325@naver.com>
2023-07-07 18:06:33 +0900

    fix: useQuery isSuccess 추가

--- src/components/pages/Article.tsx
+++ src/components/pages/Article.tsx
@@ -20,7 +20,7 @@ const Article = () => {
 
   const commentRef = useRef<HTMLTextAreaElement>(null);
 
-  const { data: articleData } = useQuery({
+  const { data: articleData, isSuccess: articleIsSuccess } = useQuery({
     queryKey: ['article'],
     queryFn: async () => {
       try {
@@ -34,7 +34,11 @@ const Article = () => {
     },
   });
 
-  const { data: commentList, refetch } = useQuery({
+  const {
+    data: commentList,
+    refetch,
+    isSuccess: commentIsSuccess,
+  } = useQuery({
     queryKey: ['comment'],
     queryFn: async () => {
       try {
@@ -94,143 +98,143 @@ const Article = () => {
 
   return (
     <Layout>
-      <div className="article-page">
-        <div className="banner">
-          <div className="container">
-            <h1>{articleData?.title}</h1>
+      {articleIsSuccess && commentIsSuccess && (
+        <div className="article-page">
+          <div className="banner">
+            <div className="container">
+              <h1>{articleData.title}</h1>
 
-            <div className="article-meta">
-              <a href="">
-                <img src={articleData?.author.image} />
-              </a>
-              <div className="info">
-                <a href="" className="author">
-                  {articleData?.author.username}
+              <div className="article-meta">
+                <a href="">
+                  <img src={articleData.author.image} />
                 </a>
-                <span className="date">
-                  {new Date(articleData?.createdAt).toLocaleDateString('en-US', dateOptions)}
-                </span>
-              </div>
-              {user.user.username !== articleData?.author.username ? (
-                <>
-                  <FollowButton
-                    username={articleData.author.username}
-                    following={articleData.author.following}
-                  />
-                  &nbsp;&nbsp;
-                  {articleData && <FavoriteButton article={articleData} isArticlePage={true} />}
-                </>
-              ) : (
-                <>
-                  <span className="ng-scope">
-                    <a
-                      className="btn btn-outline-secondary btn-sm"
-                      ui-sref="app.editor({ slug: $ctrl.article.slug })"
-                      href={`#/editor/${slug}`}
-                    >
-                      <i className="ion-edit"></i> Edit Article
-                    </a>
-                    &nbsp;&nbsp;
-                    <button className="btn btn-outline-danger btn-sm" onClick={deleteArticle}>
-                      <i className="ion-trash-a"></i> Delete Article
-                    </button>
+                <div className="info">
+                  <a href="" className="author">
+                    {articleData?.author.username}
+                  </a>
+                  <span className="date">
+                    {new Date(articleData.createdAt).toLocaleDateString('en-US', dateOptions)}
                   </span>
-                </>
-              )}
+                </div>
+                {user.user.username !== articleData.author.username ? (
+                  <>
+                    <FollowButton
+                      username={articleData.author.username}
+                      following={articleData.author.following}
+                    />
+                    &nbsp;&nbsp;
+                    {articleData && <FavoriteButton article={articleData} isArticlePage={true} />}
+                  </>
+                ) : (
+                  <>
+                    <span className="ng-scope">
+                      <a
+                        className="btn btn-outline-secondary btn-sm"
+                        ui-sref="app.editor({ slug: $ctrl.article.slug })"
+                        href={`#/editor/${slug}`}
+                      >
+                        <i className="ion-edit"></i> Edit Article
+                      </a>
+                      &nbsp;&nbsp;
+                      <button className="btn btn-outline-danger btn-sm" onClick={deleteArticle}>
+                        <i className="ion-trash-a"></i> Delete Article
+                      </button>
+                    </span>
+                  </>
+                )}
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className="container page">
-          <div className="row article-content">
-            <div className="col-xs-12">
-              <p>{articleData?.body}</p>
-              <ul className="tag-list">
-                {articleData?.tagList.map((tagData, index) => (
-                  <li key={index} className="tag-default tag-pill tag-outline">
-                    {tagData}
-                  </li>
-                ))}
-              </ul>
+          <div className="container page">
+            <div className="row article-content">
+              <div className="col-xs-12">
+                <p>{articleData.body}</p>
+                <ul className="tag-list">
+                  {articleData.tagList.map((tagData, index) => (
+                    <li key={index} className="tag-default tag-pill tag-outline">
+                      {tagData}
+                    </li>
+                  ))}
+                </ul>
+              </div>
             </div>
-          </div>
 
-          <hr />
+            <hr />
 
-          <div className="article-actions">
-            <div className="article-meta">
-              <a href="profile.html">
-                <img src={articleData?.author.image} />
-              </a>
-              <div className="info">
-                <a href="" className="author">
-                  {articleData?.author.username}
+            <div className="article-actions">
+              <div className="article-meta">
+                <a href="profile.html">
+                  <img src={articleData.author.image} />
                 </a>
-                <span className="date">
-                  {new Date(articleData?.createdAt).toLocaleDateString('en-US', dateOptions)}
-                </span>
-              </div>
-              {user.user.username !== articleData?.author.username ? (
-                <>
-                  <FollowButton
-                    username={articleData.author.username}
-                    following={articleData.author.following}
-                  />
-                  &nbsp;&nbsp;
-                  <FavoriteButton article={articleData} isArticlePage={true} />
-                </>
-              ) : (
-                <>
-                  <span className="ng-scope">
-                    <a
-                      className="btn btn-outline-secondary btn-sm"
-                      ui-sref="app.editor({ slug: $ctrl.article.slug })"
-                      href={`#/editor/${slug}`}
-                    >
-                      <i className="ion-edit"></i> Edit Article
-                    </a>
-                    &nbsp;&nbsp;
-                    <button className="btn btn-outline-danger btn-sm" onClick={deleteArticle}>
-                      <i className="ion-trash-a"></i> Delete Article
-                    </button>
+                <div className="info">
+                  <a href="" className="author">
+                    {articleData.author.username}
+                  </a>
+                  <span className="date">
+                    {new Date(articleData.createdAt).toLocaleDateString('en-US', dateOptions)}
                   </span>
-                </>
-              )}
+                </div>
+                {user.user.username !== articleData?.author.username ? (
+                  <>
+                    <FollowButton
+                      username={articleData.author.username}
+                      following={articleData.author.following}
+                    />
+                    &nbsp;&nbsp;
+                    <FavoriteButton article={articleData} isArticlePage={true} />
+                  </>
+                ) : (
+                  <>
+                    <span className="ng-scope">
+                      <a
+                        className="btn btn-outline-secondary btn-sm"
+                        ui-sref="app.editor({ slug: $ctrl.article.slug })"
+                        href={`#/editor/${slug}`}
+                      >
+                        <i className="ion-edit"></i> Edit Article
+                      </a>
+                      &nbsp;&nbsp;
+                      <button className="btn btn-outline-danger btn-sm" onClick={deleteArticle}>
+                        <i className="ion-trash-a"></i> Delete Article
+                      </button>
+                    </span>
+                  </>
+                )}
+              </div>
             </div>
-          </div>
 
-          <div className="row">
-            <div className="col-xs-12 col-md-8 offset-md-2">
-              {user.user.token ? (
-                <form className="card comment-form" onSubmit={onSubmitCommentData}>
-                  <div className="card-block">
-                    <textarea
-                      className="form-control"
-                      placeholder="Write a comment..."
-                      rows={3}
-                      ref={commentRef}
-                    ></textarea>
-                  </div>
-                  <div className="card-footer">
-                    <img src={user.user.image} className="comment-author-img" />
-                    <button className="btn btn-sm btn-primary">Post Comment</button>
-                  </div>
-                </form>
-              ) : (
-                <p style={{ display: 'inherit' }}>
-                  <a ui-sref="app.login" href="#/login">
-                    Sign in
-                  </a>
-                  &nbsp;or&nbsp;
-                  <a ui-sref="app.register" href="#/register">
-                    Sign up
-                  </a>
-                  &nbsp;to add comments on this article.
-                </p>
-              )}
+            <div className="row">
+              <div className="col-xs-12 col-md-8 offset-md-2">
+                {user.user.token ? (
+                  <form className="card comment-form" onSubmit={onSubmitCommentData}>
+                    <div className="card-block">
+                      <textarea
+                        className="form-control"
+                        placeholder="Write a comment..."
+                        rows={3}
+                        ref={commentRef}
+                      ></textarea>
+                    </div>
+                    <div className="card-footer">
+                      <img src={user.user.image} className="comment-author-img" />
+                      <button className="btn btn-sm btn-primary">Post Comment</button>
+                    </div>
+                  </form>
+                ) : (
+                  <p style={{ display: 'inherit' }}>
+                    <a ui-sref="app.login" href="#/login">
+                      Sign in
+                    </a>
+                    &nbsp;or&nbsp;
+                    <a ui-sref="app.register" href="#/register">
+                      Sign up
+                    </a>
+                    &nbsp;to add comments on this article.
+                  </p>
+                )}
 
-              {commentList &&
-                commentList.map((commentData, index) => (
+                {commentList.map((commentData, index) => (
                   <div className="card" key={index}>
                     <div className="card-block">
                       <p className="card-text">{commentData.body}</p>
@@ -244,10 +248,10 @@ const Article = () => {
                         {commentData.author.username}
                       </a>
                       <span className="date-posted">
-                        {new Date(commentData?.createdAt).toLocaleDateString('en-US', dateOptions)}
+                        {new Date(commentData.createdAt).toLocaleDateString('en-US', dateOptions)}
                       </span>
                       {!user.user.token ||
-                        (user.user.username === articleData?.author.username && (
+                        (user.user.username === articleData.author.username && (
                           <span className="mod-options">
                             <i
                               className="ion-trash-a"
@@ -260,10 +264,11 @@ const Article = () => {
                     </div>
                   </div>
                 ))}
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      )}
     </Layout>
   );
 };
